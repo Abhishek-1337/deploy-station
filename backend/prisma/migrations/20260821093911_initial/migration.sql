6aca695920230269bcc353250172ff5c19e7dc1c
Abhishek-1337 <abhishekrinku4@gmail.com>
2026-08-21 15:09:51 +0530

    prisma model for project

---
+++ backend/prisma/migrations/20260821093911_initial/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Deployment" (
+    "id" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+
+    CONSTRAINT "Deployment_pkey" PRIMARY KEY ("id")
+);
